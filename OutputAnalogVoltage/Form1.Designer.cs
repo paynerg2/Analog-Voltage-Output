c6a32b18d5990bbfbe7ae667db362c0a5f6641dd
Rodney Payne <paynerg2@gmail.com>
2018-10-15 18:34:27 -0400

    Add project files.

---
+++ OutputAnalogVoltage/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace OutputAnalogVoltage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hscVoltageValue = new System.Windows.Forms.HScrollBar();
+            this.lblScrollbarValue = new System.Windows.Forms.Label();
+            this.cmbPhysicalChannel = new System.Windows.Forms.ComboBox();
+            this.lblPhysicalChannel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // hscVoltageValue
+            // 
+            this.hscVoltageValue.Location = new System.Drawing.Point(49, 119);
+            this.hscVoltageValue.Maximum = 2009;
+            this.hscVoltageValue.Name = "hscVoltageValue";
+            this.hscVoltageValue.Size = new System.Drawing.Size(229, 27);
+            this.hscVoltageValue.TabIndex = 0;
+            this.hscVoltageValue.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hscVoltageValue_Scroll);
+            // 
+            // lblScrollbarValue
+            // 
+            this.lblScrollbarValue.AutoSize = true;
+            this.lblScrollbarValue.Location = new System.Drawing.Point(45, 103);
+            this.lblScrollbarValue.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblScrollbarValue.Name = "lblScrollbarValue";
+            this.lblScrollbarValue.Size = new System.Drawing.Size(37, 17);
+            this.lblScrollbarValue.TabIndex = 1;
+            this.lblScrollbarValue.Text = "0.0V";
+            // 
+            // cmbPhysicalChannel
+            // 
+            this.cmbPhysicalChannel.FormattingEnabled = true;
+            this.cmbPhysicalChannel.Location = new System.Drawing.Point(49, 26);
+            this.cmbPhysicalChannel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbPhysicalChannel.Name = "cmbPhysicalChannel";
+            this.cmbPhysicalChannel.Size = new System.Drawing.Size(160, 24);
+            this.cmbPhysicalChannel.TabIndex = 2;
+            // 
+            // lblPhysicalChannel
+            // 
+            this.lblPhysicalChannel.AutoSize = true;
+            this.lblPhysicalChannel.Location = new System.Drawing.Point(45, 6);
+            this.lblPhysicalChannel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPhysicalChannel.Name = "lblPhysicalChannel";
+            this.lblPhysicalChannel.Size = new System.Drawing.Size(116, 17);
+            this.lblPhysicalChannel.TabIndex = 3;
+            this.lblPhysicalChannel.Text = "Physical Channel";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 321);
+            this.Controls.Add(this.lblPhysicalChannel);
+            this.Controls.Add(this.cmbPhysicalChannel);
+            this.Controls.Add(this.lblScrollbarValue);
+            this.Controls.Add(this.hscVoltageValue);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Analog Voltage Output";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.HScrollBar hscVoltageValue;
+        private System.Windows.Forms.Label lblScrollbarValue;
+        private System.Windows.Forms.ComboBox cmbPhysicalChannel;
+        private System.Windows.Forms.Label lblPhysicalChannel;
+    }
+}
+
---
+++ OutputAnalogVoltage/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+using NationalInstruments.DAQmx;
+
+namespace OutputAnalogVoltage
+{
+    public partial class Form1 : Form
+    {
+        double voltageValue = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            hscVoltageValue.Value = 1000;
+
+            //Fill the comnbo box with the physical channels
+            cmbPhysicalChannel.Items.AddRange(DaqSystem.Local.GetPhysicalChannels(PhysicalChannelTypes.AO, PhysicalChannelAccess.External));
+
+            //Force select dev4/ao0 as the default (on my PC)
+            cmbPhysicalChannel.SelectedIndex = 2;
+        }
+
+        private void hscVoltageValue_Scroll(object sender, ScrollEventArgs e)
+        {
+            double Voltage = 0;
+            Voltage = (System.Convert.ToDouble(hscVoltageValue.Value) - 1000) / 100;
+            lblScrollbarValue.Text = Voltage.ToString("0.00") + "V";
+
+            voltageValue = Voltage;
+
+            UpdateDAQ();
+        }
+
+        private void UpdateDAQ()
+        {
+            Cursor.Current = Cursors.WaitCursor;		//Fancy cursor stuff
+            //Try-catch-finally is the error handling we will use.
+            try
+            {
+                using (Task analogWriteTask = new Task())
+                {
+                    //Create the actual channel. Min: -10V, Max: 10V, Units: V
+                    analogWriteTask.AOChannels.CreateVoltageChannel(cmbPhysicalChannel.Text, "", -10, 10, AOVoltageUnits.Volts);
+                   
+                    //Create a writer
+                    AnalogSingleChannelWriter writer = new AnalogSingleChannelWriter(analogWriteTask.Stream);
+
+
+                    //Use that writer to write a voltage value.
+                    writer.WriteSingleSample(true, voltageValue);
+                }
+            }
+            catch (DaqException ex)		// We define ex as a DaqException here.
+            {
+                MessageBox.Show(ex.Message);	// Shows whatever the exception message is if the TRY fails
+            }
+            finally		//Return the cursor to its normal state
+            {
+                Cursor.Current = Cursors.Default;
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            voltageValue = 0;
+            UpdateDAQ();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
